9b5851f6ce415ee5a67e4aaaa437e290e3890b95
codechavan <code.chavan@gmail.com>
2017-04-08 03:00:26 +0530

    Add files via upload
    Files uploaded manually.

---
+++ DMS_API_Project/DMSRepository/SQL/DocumentAccessSQL.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DMS.Repository.DAL;
+using Chavan.Logger;
+using System.Data.Common;
+using Microsoft.Practices.EnterpriseLibrary.Data;
+using DMS.Model;
+using DMS.DatabaseConstants;
+using System.Data;
+
+namespace DMS.Repository.SQL
+{
+    public class DocumentAccessSQL : DocumentAccessDAL
+    {
+        Logger logger = null;
+
+        public DocumentAccessSQL(string connectionStringName)
+            : base(connectionStringName)
+        {
+            logger = new Logger("DMS.Repository.SQL.DocumentAccessSQL");
+        }
+
+        public override FunctionReturnStatus CreateUpdateDocumentAccess(DocumentAccess paramValue)
+        {
+            Database database;
+            DbCommand dbCommand;
+            FunctionReturnStatus status = new FunctionReturnStatus();
+            try
+            {
+                DatabaseProviderFactory factory = new DatabaseProviderFactory();
+                database = factory.Create(ConnectionStringName);
+                dbCommand = database.GetStoredProcCommand(StoreProcedures.dbo.usp_Create_Update_DocumentAccess);
+
+                database.AddInParameter(dbCommand, StoreProcedures.dbo.usp_Create_Update_DocumentAccess_Parameters.SystemId, DbType.Int64, paramValue.SystemId);
+                database.AddInParameter(dbCommand, StoreProcedures.dbo.usp_Create_Update_DocumentAccess_Parameters.ObjectType, DbType.Int32, paramValue.ObjectType);
+                database.AddInParameter(dbCommand, StoreProcedures.dbo.usp_Create_Update_DocumentAccess_Parameters.ObjectId, DbType.Int64, paramValue.ObjectId);
+                database.AddInParameter(dbCommand, StoreProcedures.dbo.usp_Create_Update_DocumentAccess_Parameters.UserRoleId, DbType.Int64, paramValue.UserRoleId);
+                database.AddInParameter(dbCommand, StoreProcedures.dbo.usp_Create_Update_DocumentAccess_Parameters.CanRead, DbType.Boolean, paramValue.CanRead);
+                database.AddInParameter(dbCommand, StoreProcedures.dbo.usp_Create_Update_DocumentAccess_Parameters.CanWrite, DbType.Boolean, paramValue.CanWrite);
+                database.AddInParameter(dbCommand, StoreProcedures.dbo.usp_Create_Update_DocumentAccess_Parameters.CanDelete, DbType.Boolean, paramValue.CanDelete);
+                database.AddInParameter(dbCommand, StoreProcedures.dbo.usp_Create_Update_DocumentAccess_Parameters.CreatedBy, DbType.Int64, (paramValue.ModifiedBy <= 0 ? paramValue.CreatedBy : paramValue.ModifiedBy));
+
+                database.AddOutParameter(dbCommand, StoreProcedures.dbo.usp_Create_Update_DocumentAccess_Parameters.OutDocumentObjectUserRoleMappingId, DbType.Int64, int.MaxValue);
+                database.AddOutParameter(dbCommand, StoreProcedures.dbo.usp_Create_Update_DocumentAccess_Parameters.ErrorDescription, DbType.String, 500);
+
+                database.ExecuteNonQuery(dbCommand);
+
+                status.Data = database.GetParameterValue(dbCommand, StoreProcedures.dbo.usp_Create_Update_DocumentAccess_Parameters.OutDocumentObjectUserRoleMappingId);
+                status.Message = database.GetParameterValue(dbCommand, StoreProcedures.dbo.usp_Create_Update_DocumentAccess_Parameters.ErrorDescription).ToString();
+                if (Convert.ToInt64(status.Data) > 0)
+                {
+                    status.StatusType = StatusType.Success;
+                }
+                else
+                {
+                    status.StatusType = StatusType.Error;
+                }
+                return status;
+            }
+            catch (Exception ex)
+            {
+                logger.LogEvent(ex.ToString(), LogLevel.Error);
+                status.Message = "Error while updating document access";
+                status.StatusType = StatusType.Error;
+                return status;
+            }
+            finally
+            {
+                database = null;
+            }
+        }
+
+        public override FunctionReturnStatus RemoveDocumentAccess(DocumentAccess paramValue)
+        {
+            Database database;
+            DbCommand dbCommand;
+            FunctionReturnStatus status = new FunctionReturnStatus();
+            try
+            {
+                DatabaseProviderFactory factory = new DatabaseProviderFactory();
+                database = factory.Create(ConnectionStringName);
+                dbCommand = database.GetStoredProcCommand(StoreProcedures.dbo.usp_Remove_DocumentAccess);
+
+                database.AddInParameter(dbCommand, StoreProcedures.dbo.usp_Remove_DocumentAccess_Parameters.SystemId, DbType.Int64, paramValue.SystemId);
+                database.AddInParameter(dbCommand, StoreProcedures.dbo.usp_Remove_DocumentAccess_Parameters.ObjectType, DbType.Int32, paramValue.ObjectType);
+                database.AddInParameter(dbCommand, StoreProcedures.dbo.usp_Remove_DocumentAccess_Parameters.ObjectId, DbType.Int64, paramValue.ObjectId);
+                database.AddInParameter(dbCommand, StoreProcedures.dbo.usp_Remove_DocumentAccess_Parameters.UserRoleId, DbType.Int64, paramValue.UserRoleId);
+                database.AddInParameter(dbCommand, StoreProcedures.dbo.usp_Remove_DocumentAccess_Parameters.DeletedBy, DbType.Int64, (paramValue.ModifiedBy <= 0 ? paramValue.CreatedBy : paramValue.ModifiedBy));
+
+                database.AddOutParameter(dbCommand, StoreProcedures.dbo.usp_Create_Update_DocumentAccess_Parameters.OutDocumentObjectUserRoleMappingId, DbType.Int64, int.MaxValue);
+                database.AddOutParameter(dbCommand, StoreProcedures.dbo.usp_Create_Update_DocumentAccess_Parameters.ErrorDescription, DbType.String, 500);
+
+                database.ExecuteNonQuery(dbCommand);
+
+                status.Data = database.GetParameterValue(dbCommand, StoreProcedures.dbo.usp_Create_Update_DocumentAccess_Parameters.OutDocumentObjectUserRoleMappingId);
+                status.Message = database.GetParameterValue(dbCommand, StoreProcedures.dbo.usp_Create_Update_DocumentAccess_Parameters.ErrorDescription).ToString();
+                if (Convert.ToInt64(status.Data) > 0)
+                {
+                    status.StatusType = StatusType.Success;
+                }
+                else
+                {
+                    status.StatusType = StatusType.Error;
+                }
+                return status;
+            }
+            catch (Exception ex)
+            {
+                logger.LogEvent(ex.ToString(), LogLevel.Error);
+                status.Message = "Error while removing document access";
+                status.StatusType = StatusType.Error;
+                return status;
+            }
+            finally
+            {
+                database = null;
+            }
+        }
+
+        public override FunctionReturnStatus GetDocumentAccess(DocumentAccessSearchParameter searchParameters)
+        {
+            if (searchParameters == null)
+            {
+                searchParameters = new DocumentAccessSearchParameter();
+            }
+            Database database;
+            DbCommand dbCommand;
+            FunctionReturnStatus status = new FunctionReturnStatus();
+            try
+            {
+                DatabaseProviderFactory factory = new DatabaseProviderFactory();
+                database = factory.Create(ConnectionStringName);
+                dbCommand = database.GetStoredProcCommand(StoreProcedures.dbo.usp_Get_DocumentAccess);
+
+                database.AddInParameter(dbCommand, StoreProcedures.dbo.usp_Get_DocumentAccess_Parameters.SystemId, DbType.Int64, searchParameters.SystemId);
+                database.AddInParameter(dbCommand, StoreProcedures.dbo.usp_Get_DocumentAccess_Parameters.ObjectType, DbType.Int32, searchParameters.ObjectType);
+                database.AddInParameter(dbCommand, StoreProcedures.dbo.usp_Get_DocumentAccess_Parameters.ObjectId, DbType.Int64, searchParameters.ObjectId);
+                database.AddInParameter(dbCommand, StoreProcedures.dbo.usp_Get_DocumentAccess_Parameters.ForUserId, DbType.Int64, searchParameters.UserId);
+
+                database.AddOutParameter(dbCommand, StoreProcedures.dbo.usp_Get_DocumentAccess_Parameters.CanDelete, DbType.Boolean, int.MaxValue);
+                database.AddOutParameter(dbCommand, StoreProcedures.dbo.usp_Get_DocumentAccess_Parameters.CanRead, DbType.Boolean, int.MaxValue);
+                database.AddOutParameter(dbCommand, StoreProcedures.dbo.usp_Get_DocumentAccess_Parameters.CanWrite, DbType.Boolean, int.MaxValue);
+                database.AddOutParameter(dbCommand, StoreProcedures.dbo.usp_Get_DocumentAccess_Parameters.IsInhereted, DbType.Boolean, int.MaxValue);
+                database.AddOutParameter(dbCommand, StoreProcedures.dbo.usp_Get_DocumentAccess_Parameters.InheretedFolderId, DbType.Int64, int.MaxValue);
+                database.AddOutParameter(dbCommand, StoreProcedures.dbo.usp_Get_DocumentAccess_Parameters.InheretedFolderName, DbType.String, 250);
+                database.AddOutParameter(dbCommand, StoreProcedures.dbo.usp_Get_DocumentAccess_Parameters.OutDocumentObjectUserRoleMappingId, DbType.Int64, int.MaxValue);
+                database.AddOutParameter(dbCommand, StoreProcedures.dbo.usp_Get_DocumentAccess_Parameters.ErrorDescription, DbType.String, 500);
+
+                database.ExecuteNonQuery(dbCommand);
+
+                status.Data = database.GetParameterValue(dbCommand, StoreProcedures.dbo.usp_Create_Update_DocumentAccess_Parameters.OutDocumentObjectUserRoleMappingId);
+                status.Message = database.GetParameterValue(dbCommand, StoreProcedures.dbo.usp_Create_Update_DocumentAccess_Parameters.ErrorDescription).ToString();
+                if (Convert.ToInt64(status.Data) > 0)
+                {
+                    DocumentAccessDetail access = new DocumentAccessDetail();
+                    access.SystemId = searchParameters.SystemId;
+                    access.ObjectType = searchParameters.ObjectType;
+                    access.ObjectId = searchParameters.ObjectId;
+                    access.UserId = searchParameters.UserId;
+                    access.CanDelete = Convert.ToBoolean(database.GetParameterValue(dbCommand, StoreProcedures.dbo.usp_Get_DocumentAccess_Parameters.CanDelete));
+                    access.CanRead = Convert.ToBoolean(database.GetParameterValue(dbCommand, StoreProcedures.dbo.usp_Get_DocumentAccess_Parameters.CanRead));
+                    access.CanWrite = Convert.ToBoolean(database.GetParameterValue(dbCommand, StoreProcedures.dbo.usp_Get_DocumentAccess_Parameters.CanWrite));
+                    access.CanWrite = Convert.ToBoolean(database.GetParameterValue(dbCommand, StoreProcedures.dbo.usp_Get_DocumentAccess_Parameters.CanWrite));
+
+                    access.InheretedFolderId = Convert.ToInt64(database.GetParameterValue(dbCommand, StoreProcedures.dbo.usp_Get_DocumentAccess_Parameters.InheretedFolderId));
+                    access.IsInhereted = Convert.ToBoolean(database.GetParameterValue(dbCommand, StoreProcedures.dbo.usp_Get_DocumentAccess_Parameters.IsInhereted));
+                    access.InheretedFolderName = Convert.ToString(database.GetParameterValue(dbCommand, StoreProcedures.dbo.usp_Get_DocumentAccess_Parameters.InheretedFolderName));
+
+                    status.Data = access;
+                }
+                else
+                {
+                    status.StatusType = StatusType.Error;
+                }
+
+                return status;
+            }
+            catch (Exception ex)
+            {
+                logger.LogEvent(ex.ToString(), LogLevel.Error);
+                status.Message = "Error while fetching document access";
+                status.StatusType = StatusType.Error;
+                return status;
+            }
+            finally
+            {
+                database = null;
+            }
+        }
+
+    }
+}
---
+++ DMS_API_Project/DMSRepository/SQL/DocumentFileSQL.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DMS.Repository.DAL;
+using Chavan.Logger;
+using System.Data.Common;
+using Microsoft.Practices.EnterpriseLibrary.Data;
+using DMS.Model;
+using DMS.DatabaseConstants;
+using System.Data;
+
+namespace DMS.Repository.SQL
+{
+    public class DocumentFileSQL : DocumentFileDAL
+    {
+        Logger logger = null;
+
+        public DocumentFileSQL(string connectionStringName)
+            : base(connectionStringName)
+        {
+            logger = new Logger("DMS.Repository.SQL.DocumentFileSQL");
+        }
+
+        public override FunctionReturnStatus CreateUpdateFileWithAttribute(DocumentFile file, DocumentProperties properties)
+        {
+            Database database;
+            DbCommand dbCommand;
+            FunctionReturnStatus status = new FunctionReturnStatus();
+
+            DatabaseProviderFactory factory = new DatabaseProviderFactory();
+            database = factory.Create(ConnectionStringName);
+
+            using (DbConnection connection = database.CreateConnection())
+            {
+                connection.Open();
+                DbTransaction transaction = connection.BeginTransaction();
+
+                try
+                {
+                    dbCommand = database.GetStoredProcCommand(StoreProcedures.dbo.usp_Create_Update_DocumentFile);
+
+                    database.AddInParameter(dbCommand, StoreProcedures.dbo.usp_Create_Update_DocumentFile_Parameters.FileId, DbType.Int64, file.FileId);
+                    database.AddInParameter(dbCommand, StoreProcedures.dbo.usp_Create_Update_DocumentFile_Parameters.FileName, DbType.String, file.FileName);
+                    database.AddInParameter(dbCommand, StoreProcedures.dbo.usp_Create_Update_DocumentFile_Parameters.FileData, DbType.Binary, file.FileData);
+                    database.AddInParameter(dbCommand, StoreProcedures.dbo.usp_Create_Update_DocumentFile_Parameters.FolderId, DbType.Int64, file.FolderId);
+                    database.AddInParameter(dbCommand, StoreProcedures.dbo.usp_Create_Update_DocumentFile_Parameters.SystemId, DbType.Int64, file.SystemId);
+                    database.AddInParameter(dbCommand, StoreProcedures.dbo.usp_Create_Update_DocumentFile_Parameters.CreatedBy, DbType.Int64, (file.ModifiedBy <= 0 ? file.CreatedBy : file.ModifiedBy));
+
+                    database.AddOutParameter(dbCommand, StoreProcedures.dbo.usp_Create_Update_DocumentFile_Parameters.OutDocumentFileId, DbType.Int64, int.MaxValue);
+                    database.AddOutParameter(dbCommand, StoreProcedures.dbo.usp_Create_Update_DocumentFile_Parameters.ErrorDescription, DbType.String, 500);
+
+                    database.ExecuteNonQuery(dbCommand, transaction);
+
+                    status.Data = database.GetParameterValue(dbCommand, StoreProcedures.dbo.usp_Create_Update_DocumentFile_Parameters.OutDocumentFileId);
+                    status.Message = database.GetParameterValue(dbCommand, StoreProcedures.dbo.usp_Create_Update_DocumentFile_Parameters.ErrorDescription).ToString();
+                    if (Convert.ToInt64(status.Data) > 0)
+                    {
+                        status.StatusType = StatusType.Success;
+                        properties.DocumentFileId = Convert.ToInt64(status.Data);
+                        if (properties != null && properties.DocumentFileId > 0)
+                        {
+                            using (DocumentPropertiesSQL docProp = new DocumentPropertiesSQL(ConnectionStringName))
+                            {
+                                FunctionReturnStatus propStatus = new FunctionReturnStatus();
+                                propStatus = docProp.UpdateDocumentProperties(database, properties, transaction);
+                                if (propStatus.StatusType == StatusType.Success)
+                                {
+                                    transaction.Commit();
+                                }
+                                else
+                                {
+                                    status.StatusType = propStatus.StatusType;
+                                    status.Message = propStatus.Message;
+                                    transaction.Rollback();
+                                }
+                            }
+                        }
+                        else
+                        {
+                            transaction.Commit();
+                        }
+                    }
+                    else
+                    {
+                        status.StatusType = StatusType.Error;
+                        transaction.Rollback();
+                    }
+                    return status;
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                    logger.LogEvent(ex.ToString(), LogLevel.Error);
+                    status.Message = "Error while uploading file";
+                    status.StatusType = StatusType.Error;
+                    return status;
+                }
+                finally
+                {
+                    database = null;
+                }
+            }
+        }
+
+        public override DocumentFileSearchData GetFiles(DocumentFileSearchParameter searchParameters, PagingDetails pageDetail)
+        {
+            Database database;
+            DbCommand dbCommand;
+            try
+            {
+
+                if (searchParameters == null)
+                {
+                    searchParameters = new DocumentFileSearchParameter();
+                }
+                if (pageDetail == null)
+                {
+                    pageDetail = new PagingDetails();
+                }
+
+                DatabaseProviderFactory factory = new DatabaseProviderFactory();
+                database = factory.Create(ConnectionStringName);
+                dbCommand = database.GetStoredProcCommand(StoreProcedures.dbo.usp_Get_DocumentFiles);
+
+                database.AddInParameter(dbCommand, StoreProcedures.dbo.usp_Get_DocumentFiles_Parameters.PageIndex, DbType.Int32, pageDetail.PageIndex);
+                database.AddInParameter(dbCommand, StoreProcedures.dbo.usp_Get_DocumentFiles_Parameters.PageSize, DbType.Int32, pageDetail.PageSize);
+                database.AddInParameter(dbCommand, StoreProcedures.dbo.usp_Get_DocumentFiles_Parameters.WhereCondition, DbType.String, GetDocumentFileParameterString(searchParameters));
+                database.AddInParameter(dbCommand, StoreProcedures.dbo.usp_Get_DocumentFiles_Parameters.OrderBy, DbType.String, GetDocumentFileOrderBy(pageDetail.OrderBy));
+
+                DocumentFileSearchData LstData = new DocumentFileSearchData();
+                List<DocumentFile> lstDocuments = null;
+                using (IDataReader objReader = database.ExecuteReader(dbCommand))
+                {
+                    lstDocuments = CreateDocumentFileObject(objReader);
+                    if (objReader.NextResult())
+                    {
+                        if (objReader.Read())
+                        {
+                            LstData.RecordCount = objReader[StoreProcedures.dbo.usp_Get_DocumentFiles_Parameters.Column_RecordCount] != DBNull.Value ? Convert.ToInt64(objReader[StoreProcedures.dbo.usp_Get_DocumentFiles_Parameters.Column_RecordCount]) : 0;
+                        }
+                    }
+                }
+                LstData.LstData = lstDocuments;
+                LstData.PageDetail = pageDetail;
+                return LstData;
+            }
+            catch (Exception ex)
+            {
+                logger.LogEvent(ex.ToString(), LogLevel.Error);
+                throw new ArgumentException("Error while fetching records");
+            }
+            finally
+            {
+                database = null;
+            }
+        }
+
+        public override List<DocumentFileHistory> GetFileHistory(long documentId)
+        {
+            Database database;
+            DbCommand dbCommand;
+            try
+            {
+
+                DatabaseProviderFactory factory = new DatabaseProviderFactory();
+                database = factory.Create(ConnectionStringName);
+                dbCommand = database.GetStoredProcCommand(StoreProcedures.dbo.usp_Get_DocumentFileHistory);
+
+                database.AddInParameter(dbCommand, StoreProcedures.dbo.usp_Get_DocumentFileHistory_Parameters.DocumentFileId, DbType.Int32, documentId);
+
+                using (IDataReader objReader = database.ExecuteReader(dbCommand))
+                {
+                    return CreateDocumentFileHistoryObject(objReader);
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.LogEvent(ex.ToString(), LogLevel.Error);
+                throw new ArgumentException("Error while fetching records");
+            }
+            finally
+            {
+                database = null;
+            }
+        }
+
+        public override DocumentProperties GetFilesProperties(long documentId)
+        {
+            using (DocumentPropertiesSQL docProp = new DocumentPropertiesSQL(ConnectionStringName))
+            {
+                DocumentPropertiesSearchParameter searchParameter = new DocumentPropertiesSearchParameter();
+                searchParameter.DocumentFileId = documentId;
+
+                var propList = docProp.GetDocumentProperties(searchParameter);
+                if (propList.Count == 1)
+                {
+                    return propList[0];
+                }
+            }
+            return null;
+        }
+    }
+}
